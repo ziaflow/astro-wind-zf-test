c4e6f89e337e334a4166035fe9a39797c9545661
ziaflow <jeremy@ziaflow.com>
2025-11-28 01:28:47 -0700

    updated meta

---
+++ src/data/site.ts
@@ -0,0 +1,16 @@
+const site = {
+  siteUrl: 'https://ziaflow.com',
+  title: 'Phoenix Web Development & Digital Marketing Agency | ZiaFlow',
+  description:
+    'Custom websites, strategic SEO, PPC, and automation for Arizona service businesses. 50+ clients trust our Phoenix team for measurable growth. Book a free strategy session.',
+  logo: 'https://ziaflow.com/logo.png',
+  ogImage: 'https://ziaflow.com/_astro/default.CZ816Hke_2bosb5.jpg',
+  twitterHandle: '@ziaflow',
+  social: {
+    linkedin: 'https://www.linkedin.com/company/ziaflow',
+    facebook: 'https://www.facebook.com/your-page',
+    twitter: 'https://twitter.com/your-handle'
+  }
+};
+
+export default site;
